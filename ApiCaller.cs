df4938ed007d1a0bd886473055f30d5bab2eb0f4
OMid Pirgَazi <39760536+pirgazi@users.noreply.github.com>
2020-01-20 10:50:56 +0330

    first

---
+++ ApiCaller.cs
@@ -0,0 +1,180 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiCaller
+{
+    public class Call
+    {
+        public static T Patch<T, T2>(string url, T2 model)
+        {
+            try
+            {
+                #region ارسال پیام و دریافت نتیجه ارسال
+
+                ASCIIEncoding encoder = new ASCIIEncoding();
+                String postData = JsonConvert.SerializeObject(model);
+                byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+                request.Method = "PATCH";
+                request.KeepAlive = false;
+                request.ContentType = "application/json";
+                //Request.Headers.Add(string.Format("Authorization: key={0}", serverkey));
+                request.ContentLength = byteArray.Length;
+
+                Stream dataStream = request.GetRequestStream();
+                dataStream.Write(byteArray, 0, byteArray.Length);
+                dataStream.Close();
+
+                HttpWebResponse Response = request.GetResponse() as HttpWebResponse;
+
+                Stream receiveStream = Response.GetResponseStream();
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+
+                StreamReader Reader = new StreamReader(Response.GetResponseStream());
+
+                var responseLine = Reader.ReadToEnd();
+
+                var rec = JsonConvert.DeserializeObject<T>(responseLine);
+
+                return rec;
+                #endregion
+
+            }
+            catch (Exception ee)
+            {
+                return default(T);
+            }
+        }
+
+        public static T Put<T, T2>(string url, T2 model)
+        {
+            try
+            {
+                #region ارسال پیام و دریافت نتیجه ارسال
+
+                ASCIIEncoding encoder = new ASCIIEncoding();
+                String postData = JsonConvert.SerializeObject(model);
+                byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+                request.Method = "PUT";
+                request.KeepAlive = false;
+                request.ContentType = "application/json";
+                //Request.Headers.Add(string.Format("Authorization: key={0}", serverkey));
+                request.ContentLength = byteArray.Length;
+
+                Stream dataStream = request.GetRequestStream();
+                dataStream.Write(byteArray, 0, byteArray.Length);
+                dataStream.Close();
+
+                HttpWebResponse Response = request.GetResponse() as HttpWebResponse;
+
+                Stream receiveStream = Response.GetResponseStream();
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+
+                StreamReader Reader = new StreamReader(Response.GetResponseStream());
+
+                var responseLine = Reader.ReadToEnd();
+
+                var rec = JsonConvert.DeserializeObject<T>(responseLine);
+
+                return rec;
+                #endregion
+
+            }
+            catch (Exception ee)
+            {
+                return default(T);
+            }
+        }
+
+        public static T Post<T, T2>(string url, T2 model)
+        {
+            try
+            {
+                #region ارسال پیام و دریافت نتیجه ارسال
+
+                ASCIIEncoding encoder = new ASCIIEncoding();
+                String postData = JsonConvert.SerializeObject(model);
+                byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+                request.Method = "POST";
+                request.KeepAlive = false;
+                request.ContentType = "application/json";
+                //Request.Headers.Add(string.Format("Authorization: key={0}", serverkey));
+                request.ContentLength = byteArray.Length;
+
+                Stream dataStream = request.GetRequestStream();
+                dataStream.Write(byteArray, 0, byteArray.Length);
+                dataStream.Close();
+
+                HttpWebResponse Response = request.GetResponse() as HttpWebResponse;
+
+                Stream receiveStream = Response.GetResponseStream();
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+
+                StreamReader Reader = new StreamReader(Response.GetResponseStream());
+
+                var responseLine = Reader.ReadToEnd();
+
+                var rec = JsonConvert.DeserializeObject<T>(responseLine);
+
+                return rec;
+                #endregion
+
+            }
+            catch (Exception ee)
+            {
+                return default(T);
+            }
+        }
+
+        public static T Get<T>(string url)
+        {
+            try
+            {
+                #region ارسال پیام و دریافت نتیجه ارسال
+                ASCIIEncoding encoder = new ASCIIEncoding();
+
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+                request.Method = "GET";
+
+                HttpWebResponse Response = request.GetResponse() as HttpWebResponse;
+                Stream receiveStream = Response.GetResponseStream();
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+
+                StreamReader Reader = new StreamReader(Response.GetResponseStream());
+
+                var responseLine = Reader.ReadToEnd();
+
+                return JsonConvert.DeserializeObject<T>(responseLine);
+
+                #endregion
+
+            }
+            catch (Exception ee)
+            {
+                return default(T);
+            }
+        }
+    }
+}
